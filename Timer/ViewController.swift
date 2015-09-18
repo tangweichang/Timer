a0ea3dd34f2eab1a30f576cbe1de4d8932dae7a8
tangweichang <tangweichangjlu@gmail.com>
2015-09-18 13:30:55 -0400

    Timer iOS9

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    func increaseTimer() {
+        time++
+        timerLabel.text = String(time) // or ="\(time)"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        timerLabel.text = "0"
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
